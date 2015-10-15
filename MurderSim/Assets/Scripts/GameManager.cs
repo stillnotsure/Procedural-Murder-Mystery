00d0ea10b865af631a381effb1065c881c6101ae
Andrew Stockdale <as834@kent.ac.uk>
2015-10-15 18:05:16 +0100

    Empty room with walls
    Created the GameManager and BoardManager, creates an empty room using a
    tileset.

---
+++ MurderSim/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+
+	// Use this for initialization
+	void Start () {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
