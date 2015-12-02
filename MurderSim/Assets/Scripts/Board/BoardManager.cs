54728dc65d2f24d039722a95e2f4e7629baaea1c
Andrew Stockdale <as834@kent.ac.uk>
2015-12-02 20:34:37 +0000

    Mansion ingame
    Redid tilemap in Tiled (good riddance TileSprite), appears ingame

---
+++ MurderSim/Assets/Scripts/Board/BoardManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using SpriteTile;
+
+public class BoardManager : MonoBehaviour {
+
+    public Camera tileCam;
+    public TextAsset level;
+
+    void Awake () {
+        Tile.SetCamera(tileCam);
+        Tile.LoadLevel(level);
+        Tile.SetLayerPosition(1, new Vector2(-10.0f, -3.7f));
+        Tile.SetLayerPosition(0, new Vector2(10.0f, 6.6f));
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MurderSim/Assets/Scripts/Legacy/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+    public ConversationScript conversationScript;
+
+	// Use this for initialization
+	void Start () {
+        boardScript = GetComponent<BoardManager>();
+        conversationScript = GetComponent<ConversationScript>();
+        InitGame();
+	}
+	
+    void InitGame()
+    {
+        //boardScript.SetupScene();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Debug();
+	}
+
+    void Debug()
+    {
+        if (Input.GetKeyDown("`"))
+        {
+            conversationScript.displayDebugText();
+        }
+    }
+}
